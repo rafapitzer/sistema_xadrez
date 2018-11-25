f5368c182c34efb257136870d5518aec5b1fe444
rafapitzer <rafa_thadeupitzer@hotmail.com>
2018-11-25 15:00:33 -0200

    Primeira Classe Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import camada_tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Posicao pos = new Posicao(3, 4);
+		
+		System.out.println(pos);
 	}
 
 }
